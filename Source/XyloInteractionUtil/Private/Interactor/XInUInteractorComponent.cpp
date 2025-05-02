0d3f35b329a7472ff892acc1765a7e9e96621e86
XyloIsCoding <xylo.is.coding@gmail.com>
2025-05-02 15:08:49 +0200

    copyright notice

--- Source/XyloInteractionUtil/Private/Interactor/XInUInteractorComponent.cpp
+++ Source/XyloInteractionUtil/Private/Interactor/XInUInteractorComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2025, Davide Bosco. All Rights Reserved.
 
 #include "Interactor/XInUInteractorComponent.h"
 
